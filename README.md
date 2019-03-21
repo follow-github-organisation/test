aee078cbcb2a99c6e16e9da5532828d4cc5a1972
Pravendra Singh <hackpravj@gmail.com>
2019-03-21 19:33:19 +0530

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-followgithub.org
-================
-
-> WIP
---
+++ precache-manifest.17db9451285f17452a4e46b54b1aa2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7136e44d2c8904b350a6",
+    "url": "/static/js/main.283bc444.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "7136e44d2c8904b350a6",
+    "url": "/static/css/main.7bbe04ca.chunk.css"
+  },
+  {
+    "revision": "b4541a6fd794998d6ece9116f5cff6e3",
+    "url": "/index.html"
+  }
+];
